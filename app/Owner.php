00246bca25d30bf20a55d053e9d8c3136ca5964e
vagrant <vagrant@vet>
2020-08-11 16:00:00 +0000

    Implement Owner->fullName() method

--- app/Owner.php
+++ app/Owner.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
 {
-    //
+    public static function haveWeBananas($number)
+    {
+        if ($number === 0) {
+            return "No we have no bananas";
+        } else if ($number === 1) {
+            return "Yes we have 1 banana";
+        }
+        return "Yes we have {$number} bananas";
+    }
+
+    public function fullName(): string
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
