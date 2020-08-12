977359946b8d624185f0358468c0dad841747a23
Zack Adlington <zadlington@gmail.com>
2020-08-12 10:05:02 +0100

    Add test for creating Owner object

---
+++ tests/Unit/OwnerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Owner;
+use PHPUnit\Framework\TestCase;
+
+class OwnerTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->owner = new Owner;
+        $this->owner->first_name = "Watchy";
+        $this->owner->last_name = "McWatchface";
+        $this->owner->telephone = "0121DOONE";
+        $this->owner->email = "f91w@casio.com";
+    }
+
+    public function testFieldsAreCorrect()
+    {
+        $this->assertSame($this->owner->first_name, "Watchy");
+        $this->assertSame($this->owner->last_name, "McWatchface");
+        $this->assertSame($this->owner->telephone, "0121DOONE");
+        $this->assertSame($this->owner->email, "f91w@casio.com");
+    }
+}
