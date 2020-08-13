49126beb883b8233b83ffadc4d7783b738cbb455
Zack Adlington <zadlington@gmail.com>
2020-08-13 11:53:56 +0100

    Add timeOfDay greeting to Welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section("title") "Vet House Paradiso" @endsection
 
 @section("content")
-<h1>Welcome</h1>
+<h1>Good {{ $timeOfDay }}!</h1>
 <p>Welcome to the Vet House Paradiso.<p>
         <img src="https://a.ltrbxd.com/resized/sm/upload/k6/2s/3l/85/guest-house-paradiso-1200-1200-675-675-crop-000000.jpg?k=fd7aaeaa17"
             alt="Guest House Paradiso still" width="200px" />
