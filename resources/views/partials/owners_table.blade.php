d09e587282367023a8d8899321f070c380535908
Zack Adlington <zadlington@gmail.com>
2020-08-13 11:26:05 +0100

    Create msg in owners_table view if none exist

--- resources/views/partials/owners_table.blade.php
+++ resources/views/partials/owners_table.blade.php
@@ -1,12 +1,16 @@
+@if (count($owners) === 0)
+<em>There are no owners sad_face.jpg</em>
+@else
+@foreach ($owners as $owner)
 <div class="list-group">
 
-    @foreach ($owners as $owner)
     <a href="/owners/{{ $owner->id }}" class="list-group-item 􏰀→ list-group-item-action">
         <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">{{ $owner->fullName() }}</h5>
-
             <small>{{ $owner->fullAddress() }}</small>
         </div>
 
     </a>
-    @endforeach </div>
+</div>
+@endforeach
+@endif
