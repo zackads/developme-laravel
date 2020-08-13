34897889998c2db802d6d65cd12ee03363db6f82
Zack Adlington <zadlington@gmail.com>
2020-08-13 11:53:27 +0100

    Route / to Home controller

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "Home@index");
 
 Route::get('/owners', "Owners@index");
