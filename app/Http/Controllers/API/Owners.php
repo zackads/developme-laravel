d62226485fe99bdbd87a29824ed30f662c170408
Zack Adlington <zadlington@gmail.com>
2020-08-16 20:01:22 +0100

    Create Owners API

---
+++ app/Http/Controllers/API/Owners.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Owner;
+use Illuminate\Http\Request;
+
+class Owners extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Owner::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        return Owner::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Owner $owner)
+    {
+        return $owner;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Owner $owner)
+    {
+        $data = $request->all();
+
+        $owner->fill($data)->save();
+
+        return $owner;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Owner $owner)
+    {
+        $owner->delete();
+
+        return response(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\Owners;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,29 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get("/owners", [Owners::class, "index"]);
+
+Route::group(["prefix" => "owners"], function () {
+    // GET /owners: show all Owners
+    Route::get("", [Owners::class, "index"]);
+
+    // POST /owners: create a new owner
+    Route::post("", [Owners::class, "store"]);
+
+    // all these routes also have an owner ID in the // end-point, e.g. /owners/8
+    Route::group(["prefix" => "{owner}"], function () {
+        // GET /Owners/8: show the Owner
+        Route::get("", [Owners::class, "show"]);
+
+        // PUT /Owners/8: update the Owner
+        Route::put("", [Owners::class, "update"]);
+
+        // DELETE /Owners/8: delete the owner
+        Route::delete("", [Owners::class, "destroy"]);});
 });
