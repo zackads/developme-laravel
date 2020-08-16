e6b77bbefab61e81fc22c25510d686f852624458
Zack Adlington <zadlington@gmail.com>
2020-08-16 14:49:16 +0100

    Create database factory for Animal model

---
+++ database/factories/AnimalFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Animal;
+use Faker\Generator as Faker;
+
+$factory->define(Animal::class, function (Faker $faker) {
+    $names = ["Loki", "Spot", "Sparky", "Lassie", "Rocket", "Rupert", "Yogi"];
+    $types = ["cat", "dog", "hamster", "gerbil", "bear", "tiger", "crocodile"];
+
+    return [
+        "name" => $names[array_rand($names, 1)],
+        "type" => $types[array_rand($types, 1)],
+        "dob" => $faker->date($format = 'Y-m-d', $max = 'now'),
+        "weight" => rand(10, 100) / 10,
+        "height" => rand(10, 30) / 10,
+        "biteyness" => rand(1, 5),
+    ];
+});
