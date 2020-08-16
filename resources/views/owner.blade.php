e7a5f199b91f64b5c63f889c026af4a7fc71df04
Zack Adlington <zadlington@gmail.com>
2020-08-16 15:55:13 +0100

    Add animal details to owner record template

--- resources/views/owner.blade.php
+++ resources/views/owner.blade.php
@@ -4,6 +4,22 @@
     <h2 class="card-header">{{ $owner->fullName() }}</h2>
     <article class="card-body">
         {{ $owner->fullAddress() }}
+        <h3>Animals</h3>
+        <ul>
+            @foreach ($owner->animals as $animal)
+            <li>
+                Name: {{ $animal->name }}
+                <ul>
+                    <li>Type: {{ $animal->type}}</li>
+                    <li>Date of birth: {{ $animal->dob}}</li>
+                    <li>Weight {{ $animal->weight}}</li>
+                    <li>Height: {{ $animal->height}}</li>
+                    <li>Biteyness: {{ $animal->biteyness}}</li>
+                    <li>Dangerous: {{ $animal->dangerous() ? "True" : "False" }}</li>
+                </ul>
+            </li>
+            @endforeach
+        </ul>
     </article>
 </div>
 @endsection
