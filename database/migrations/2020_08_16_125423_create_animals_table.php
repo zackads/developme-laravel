80c46597436304285e1c6cbb462a83ba04084721
Zack Adlington <zadlington@gmail.com>
2020-08-16 14:07:16 +0100

    Create Animal migration

---
+++ database/migrations/2020_08_16_125423_create_animals_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            // Model fields
+            $table->id();
+            $table->timestamps();
+            $table->string("name", 100);
+            $table->date("dob");
+            $table->decimal("weight", 6, 2);
+            $table->decimal("height", 6, 2);
+            $table->integer("biteyness");
+
+            // Foreign key
+            $table->foreignId("owner_id");
+
+            // Foreign key constraint
+            $table->foreign("owner_id")->references("id")
+                ->on("owners")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
